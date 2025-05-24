7025dd438d3e584388a4262bf75d072169260ed0
monikrab <arcticstrokes76@gmail.com>
2025-05-24 12:25:12 +0100

    Updated blog.hs

--- blog.hs
+++ blog.hs
@@ -1,27 +1,44 @@
 -- Haskell HTML Blog Generator
 
--- Entry point (main function)
+main :: IO ()
 main = putStrLn myHTML
---   = putStrLn (wrapHtml "Hello, world!")  also works
 
 -- Order of definition doesn't matter, dependencies can be defined below their usage
-myHTML = makeHTML "My title" "My content"
+myHTML :: String
+myHTML = makeHTML "Monikrab's Haskell Blog" myContent
 
--- All functions take one argument and return one value, like mathematical functions
-makeHTML :: String -> (String -> String)
+makeHTML :: String -> String -> String
 makeHTML title content =
     html_ (head_ (title_ title) <> body_ content)
--- makeHtml takes the string input into `title` and returns the function makeHtml "title" 
--- makeHtml "title" takes the input into `content` and returns the final string defined in the expression
+    
+-- All functions take one argument and return one value, like mathematical functions
+-- This is because Haskell actually reads lambdas, which are anonymous functions with a single argument
+-- The code above works like the following:
+--
+-- `title` is replaced with "My title", we now have `makeHTML "My title" content`
+-- `content` is replaced with "My content", we now have `makeHTML "My title" "My content"`
+-- the function returns and the value is stored into myHTML
 
+html_  :: String -> String
+body_  :: String -> String
+head_  :: String -> String
+title_ :: String -> String
+h1_    :: String -> String
+code_  :: String -> String
 
 -- Function definition: name arg1 argN = expr
 html_  = element "html"
 body_  = element "body"
 head_  = element "head"
 title_ = element "title"
+p_     = element "p"
+h1_    = element "h1"
+code_  = element "code"
+-- In an imperative language:
+-- html(content) = element("html")
+-- `html_` is now the same as calling element with the first argument "html"
 
-element :: String -> String -> String
+--element :: String -> String -> String
 element tag content =
     "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 -- The same thing happens here,
@@ -32,3 +49,10 @@ element tag content =
 -- z <> (y <> x)
 -- Append y with x and z with y <> x
 
+myContent =
+    h1_ "How to make a function in Haskell"
+    <> p_ "To make a function in Haskell, you have two options:"
+    <> p_ " - Named functions" <> p_ " - Lambda (anonymous) functions"
+    <> p_ "A regular (named) function is written like so:" <> code_ "function arg1 arg2 argN = expression"
+    <> p_ "A lambda function is written like so:" <> code_  "\\arg -> expression"
+    <> p_ "You can also write multi-parameter lambdas:" <> code_ "\\arg1 arg2 argN -> expression"
